eb6fa0aa15af1dd3904c7c91e20a1cb7a865b6f8
Izabela Leme <izabela.leme@plasutil.com.br>
2024-06-28 17:37:32 -0300

    New discount tests

--- src/JornadaMilhasV1/Modelos/OfertaViagem.cs
+++ src/JornadaMilhasV1/Modelos/OfertaViagem.cs
@@ -7,13 +7,32 @@ using JornadaMilhasV1.Validador;
 
 namespace JornadaMilhasV1.Modelos;
 
-public class OfertaViagem: Valida
+public class OfertaViagem : Valida
 {
+    private double desconto;
+
     public int Id { get; set; }
-    public Rota Rota { get; set; } 
+    public Rota Rota { get; set; }
     public Periodo Periodo { get; set; }
     public double Preco { get; set; }
 
+    public const double Desconto_Maximo = 0.7;
+    public double Desconto
+    {
+        get => desconto;
+        set
+        {
+            desconto = value;
+            if (desconto >= Preco)
+            {
+                Preco *= (1 - Desconto_Maximo);
+            }
+            else
+            {
+                Preco -= desconto;
+            }
+        }
+    }
 
     public OfertaViagem(Rota rota, Periodo periodo, double preco)
     {
@@ -33,10 +52,11 @@ public class OfertaViagem: Valida
         if (!Periodo.isValid)
         {
             Erros.RegistrarErro(Periodo.Erros.Sumario);
-        } else if (Rota == null || Periodo == null)
+        }
+        else if (Rota == null || Periodo == null)
         {
             Erros.RegistrarErro("A oferta de viagem não possui rota ou período válidos.");
-        } 
+        }
         else if (Preco <= 0)
         {
             Erros.RegistrarErro("O preço da oferta de viagem deve ser maior que zero.");
